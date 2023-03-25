bcc152516a37bb6cc6e361a2940b94616e6ba2d1
diminuen <dawid.dobosz.stud@pw.edu.pl>
2023-03-25 11:07:23 +0100

    Initial commit

---
+++ src/main/java/pl/diminuen/propertysalessystem/PropertySalesSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.diminuen.propertysalessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/diminuen/propertysalessystem/PropertySalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.diminuen.propertysalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
